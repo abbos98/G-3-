31f1539a32ab0103c7b16ab94deeea65578bbb4d
Abbos <abbos.pankiy2018@gmail.com>
2024-07-22 18:17:06 +0900

    String_methods

---
+++ lesson8_starting_javascript/script.js
@@ -0,0 +1,40 @@
+// parseInt(); => qoldiqsiz qoldiradi!
+
+// let num = 15.40;
+
+// console.log(parseInt(num));
+
+
+
+//parseFloot(); => qoldiqli qilib ciqarib beradi!
+
+// let num = 15.69;
+
+// console.log(parseFloat(num));
+
+
+
+// toFixed(); => kereklik qisimdagi qoldiqni belgilab chiqarish!
+
+//  let num = 15.43;
+
+//  console.log(num.toFixed(1));
+
+
+
+// Stringni numberga ozgartirish! (2ta usul);
+
+// 1 //
+
+// let num = '50';
+
+// console.log(+num);
+
+// 2 //
+
+// let num = '200';
+
+// console.log(parseInt(num));
+
+
+
