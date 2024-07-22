4cdc5d7eb2282696b2b21e807a4365b5d1de07d7
Abbos <abbos.pankiy2018@gmail.com>
2024-07-22 18:18:40 +0900

    Math_methods vs questions

---
+++ lesson9/script.js
@@ -0,0 +1,103 @@
+// Math.pow() => berilgan sonni kubini topib beradi!
+
+// console.log(Math.pow(3, 3));
+
+
+// Math.sqrt(); => berilgan soni ildizini chiqarib beradi!
+
+// console.log(Math.sqrt(36));
+
+
+//Math.cbrt(); => berilgan soni 3-darjali ildizini ciqarib beradi (kubni teskari holati)!
+
+// console.log(Math.cbrt(8));
+
+
+//Math.min vs Math.max => berilgan sonlarni kotta yoki kichkinasini olib beradi!
+
+// console.log(Math.min(24,67,34,93));
+
+// console.log(Math.max(56,87,23,65,13,));
+
+
+// console.log(parseInt(Math.random()*10));
+
+
+
+// << eval >> => String typedagi math amalarni bajarishiga imkoniyat beradi!
+
+// let num = '2 + 2';
+
+// console.log(eval(num));
+
+
+// let num = 4 + 4;
+
+// console.log(typeof num.toString());
+
+
+//////////////////////////////////////////////////////////////////////////
+
+// "", '', ``   backtic and others
+
+// let name = 'Abbos';
+
+// console.log(`name: ${name}`);
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+// how to find index of text
+
+
+// let str = `Begmatov Abbos`;
+
+// console.log(str[0]);
+
+
+// indexOf(); 
+
+// let name = 'Abbos';
+
+// console.log(name.indexOf('A'));
+
+
+
+
+// 15 ta masala
+
+// 1) Berilgan qiymatlar ichida eng katta miqdorni aniqlash uchun qaysi methodni ishlatsak bo'ladi?
+
+// 2) 10 ta insonni random holatda tanlash uchun nima qilishimiz kerak? (qoldiqsiz holatda son chiqishi kerak!)
+
+// 3) String holatida matematik amalni bajaring va typeni qayta stringga o'giring!
+
+// 4) Stringda berilgan qiymatni indexni aniqlash uchun qaysi uslubdan foydalansak boladi?
+
+// 5) console.log(3 ** 2) => shu holatni Mathning qaysi methodi orqali bajarsa boladi?
+
+// 6) console.log(str.indexOf())  va console.log(str[]) ning nima farqi bor?
+
+// 7) Uyimdan institugacha 560 mets va institudan ishxonamgacha 430m jami nechi metr?
+
+// 8) NaN va underfined qiymatlarining farqi bormi?
+
+// 9) Math.random methodni ishlating va random bolgan songa 10 sonini qoshib qoying (qoldiqsiz son bolishi kerak)!
+
+// 10) Stringda berilgan textni hammas harflarini katta yokida hammasini kichkinaga aylantiring!
+
+// 11) Number type biror bir qiymatni berib uni string typega va qayta numberga ogirib bering!
+
+// 12) 2ta ozgaruvchi ochib birini maxsimal sonini toping ikkinchisni esa minumum sonini toping va ularni bir-biriga qoshib bering!
+
+// 13) Nechta string methodlarini bilaszi va ular qaysilar?
+
+// 14) Musbat sonlarini chiqmasligi uchun qaysi methoddan foydalansak bo'ladi?
+
+// 15) 36 sonini ildiz ostida ciqarib va unga 10 sonini qoshib qoysak nechi boladi?
+
+
+
+
+
+
