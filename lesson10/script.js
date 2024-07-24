1162f705119e0fc118079803a6dd0d5170111b6a
Abbos <abbos.pankiy2018@gmail.com>
2024-07-24 19:53:06 +0900

    functions vs hometasks

--- lesson10/script.js
+++ lesson10/script.js
@@ -79,7 +79,32 @@
 
 
 
+// Arrow function
+
+// const webBrain = (a) => {
+//     return a * a;
+// }
+// console.log(webBrain(2));
+
+
+// function 1 qatorlik bolsa unga return qoymasa ham boladi!
+
+// const webBrain = (a) => a * a;
+
+// console.log(webBrain(2));
+
+
+
+// const hello = (name) => {
+//     console.log(`Hello ${name}`);
+// }
+
+// hello('Abbos');
+
+
+
 // =============================================================================================
+// <<< with declaration function >>>
 
 // 1st function ichida 20 sonini 3ga bo'ling va qoldiq sonlarni remove qilib tashlang!
 
@@ -94,7 +119,7 @@
 // ===============================================================================
 
 
-// 2nd function ichida musbat sonli qiymatni oddiy qiymatga aylantirib bering!
+// 2nd function ichida musbat sonli qiymatni oddiy qiymatga aylantirib bering(ya'ni minus son chiqmasin!
 
 // function webBrain() {
 //     let firstNum = 50;
@@ -106,7 +131,7 @@
 // ===============================================================================
 
 
-// 3th function ichida shunday qiymat beringki ular ism, familya, yosh haqida malumot bolsin va ular bir nechta inlarniki bolsin!
+// 3th function ichida shunday qiymat beringki ular ism, familya, yosh haqida malumot bo'lsin va ular bir nechta insonlarniki bolsin!
 
 // function webBrain (ism, familya, yosh) {
 //     console.log(`Salom ${ism} ${familya} sizning yoshingiz ${yosh}da`);
@@ -128,8 +153,8 @@
 // ====================================================================================
 
 
-// 5th funtion ichida sahifaga kirish yani parol login terish orqali sahifaga kirishni bajaring
-// (Agar togri bolsa sahifangizga xush kelibsiz deb agar xato bolsa id yoki parol xato deb ciqsin)
+// 5th funtion ichida sahifaga kirish yani parol login terish orqali sahifaga kirishni bajaring!
+// (Agar togri bolsa sahifangizga xush kelibsiz deb agar xato bolsa id yoki parol xato deb ciqsin!)
 
 // function webBrain() {
 //     let id = 'Abbos'
@@ -144,7 +169,7 @@
 // ===================================================================================
 
 
-// 6th function ichida Abbos nomili ozgaruvchini Murodilga ozgartiring hamda Murodil sozi 5 marotaba qaytarilsin!
+// 6th function ichida Abbos nomili o'zgaruvchini Murodilga o'zgartiring hamda Murodil sozi 5 marotaba qayta-qayta chiqsin!
 
 // function webBrain() {
 //     let name = 'Abbos';
@@ -196,3 +221,117 @@
 //     console.log(name.length);
 // }
 // webBrain();
+// ===================================================================================
+
+// ===================================================================================
+
+
+// <<< with arrow function >>>
+
+// 11th function ichidagi textni bosh va oxirgi harfini remove qilib bering!
+
+// const cut = (title) => {
+//    return title.slice(1, -1)
+// }
+// console.log(cut('Abbos'));
+// =====================================================================================
+
+
+
+// 12th function ichida berilgan texni teskari holatda chiqarib bering va qayta stringga otqazing!
+
+// const click = (name) => {
+//     return name.split("").reverse().toString();
+// }
+// console.log(click('Abbos'));
+// =====================================================================================
+
+
+// 13th function ichida berilgan sonlarni ketma ketligi boyicha qo'yib bering!
+
+// const numbers = () => {
+
+//    let arr = [4,2,3,5,1];
+   
+//    console.log(arr.sort((a,b) => a - b));
+// }
+
+// numbers();
+// =========================================================================================
+
+
+// 14th function ichida Abbos sozida o harifi nechinchi indexdaligini topib bering!
+
+// const find = (name) => {
+//     return name.search('o');
+// }
+
+// console.log(find('Abbos'));
+// =========================================================================================
+
+
+
+//15th function ichidagi textning hariflarini alfavit holatda sort qilib bering!
+
+// const touch = (laters) => {
+//     let text = ['s', 'b', 'a'];
+    
+//     console.log(text.sort((a, b) => a.localeCompare(b)));
+// }
+// touch();
+// ==========================================================================================
+
+
+// 16th strinda bolgan raqamlar orqali matematik amal bajaring!
+
+// const some = () => {
+//     let num = '2 + 2'
+//     console.log(eval(num));
+// }
+// some();
+// =========================================================================================================
+
+
+// 17th function ichida shunday kod yozing 10gacha bo'lgan random soni chiqsin (sonlar qoldiqsiz holatda chiqsin!
+
+// const rand = () => {
+//     return parseInt(Math.random() * 10);
+// }
+// console.log(rand());
+// =========================================================================================================
+
+// 18th function ichcidagi qiymatning white spacelarini yoqotib va oxiriga A harifini qoshib qoying!
+
+// const click = (name) => {
+//     return name.trim().padEnd(6,'A');
+// }
+// console.log(click('Abbos     '));
+// ==========================================================================================================
+
+
+// 19th function ichida Abbos sozidan o harfni nechinchi indexda turgnaini topib bering!
+
+// const click = () => {
+//     let index = 'Abbos';
+
+//     console.log(index.charAt(3));
+// }
+// click();
+// =============================================================================================
+
+
+
+// 20th function ichida berilgan texning eng oxirgi harfini olib bering!
+// const num = (name) => {
+   
+// let str = 'Adfsgjdfngljksdfkgjasfsadf';
+
+// console.log(str[str.length -1]);
+// }
+
+// num();
+
+
+
+
+
